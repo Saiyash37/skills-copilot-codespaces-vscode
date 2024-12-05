50d1eedc8fc2e5af40e44b38abe037a5e5bee935
YashpreetSingh GurmelSingh Maan <yashpreetsingh@mail.ca.in>
2024-12-05 08:08:23 +0000

    added members js file step 3

---
+++ members.js
@@ -0,0 +1,14 @@
+function skillsMembers () {
+  return {
+    members: [
+      {
+        name: 'John Doe',
+        skills: ['JavaScript', 'React', 'Node']
+      },
+      {
+        name: 'Jane Doe',
+        skills: ['JavaScript', 'Vue', 'Node']
+      }
+    ]
+  }
+}
